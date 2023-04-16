0c3335d03584e85003d3dbfae819682fe6160c82
farhanlumoshive <farhan.arfianto@lumoshive.com>
2023-04-16 23:31:17 +0700

    add livewire component on app layout

---
+++ app/Http/Livewire/HeaderNotification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HeaderNotification extends Component
+{
+    public function render()
+    {
+        return view('livewire.header-notification');
+    }
+}
---
+++ app/Http/Livewire/RightBar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RightBar extends Component
+{
+    public function render()
+    {
+        return view('livewire.right-bar');
+    }
+}
